70678e16500d8ffd7982d51573a317460a952c1b
Terrance Peters <terrancejpeters@gmail.com>
2018-11-22 02:33:28 -0500

    Small UI fixes

--- ANAV/search.py
+++ ANAV/search.py
@@ -10,7 +10,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-bp = Blueprint('search', __name__, url_prefix='/search')
+bp = Blueprint('search', __name__, static_folder="../", url_prefix='/search')
 
 @bp.route('/', methods=('GET', 'POST'))
 def search():
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 export FLASK_APP=ANAV
 export FLASK_ENV=development
 flask run
