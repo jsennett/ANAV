7ed675c05c67936de05fbd6607171c635b2cb337
Joshua Sennett <joshua.sennett@tufts.edu>
2018-12-01 19:39:58 -0500

    Adjusted queries for database changes

--- tests/test_database_utils.py
+++ tests/test_database_utils.py
@@ -1,8 +1,57 @@
 import pytest
+import sys
+import time
+
+sys.path.append('../')
 from data.database_utils import nodes_within_radius, nearest_node
+from data.config import credentials
 
 
 def test_nearest_node():
-    """ Placeholder test """
-    nn = nearest_node(credentials, lat=42.359055, lon=-71.093500, meters=100)
-    assert(nn != None)
+    """ Test if a nearest node can be found """
+    sample_lat, sample_lon = 42.299944, -72.588062
+    nn = nearest_node(credentials, lat=sample_lat, lon=sample_lon, meters=100)
+
+    # make sure we get the right return type
+    assert(type(nn) == tuple)
+
+    # make sure each node is a row from edges
+    assert(len(nn) == 10)
+
+    # make sure we are within .1 degree of latitude and longitude
+    assert(abs(float(nn[1]) - sample_lat) < .1 and abs(float(nn[2]) - sample_lon) < .1)
+
+
+def test_nn_out_of_range():
+    """ Test if a nearest node can be found with coordinates out of range """
+    nn = nearest_node(credentials, lat=100, lon=-200, meters=100)
+
+    # make sure we get the right return type
+    assert(type(nn) == tuple)
+
+    # assert we have not found a point
+    assert(len(nn) == 0)
+
+
+def test_nodes_in_radius():
+    """ Test a standard query for nodes in a radius """
+    sample_lat, sample_lon = 42.299944, -72.588062
+    nodes = nodes_within_radius(credentials, lat=sample_lat, lon=sample_lon, radius=.05)
+
+    # make sure we get the right return type
+    assert(type(nodes) == list)
+
+    # make sure we get a reasonable amount of nodes
+    assert(len(nodes) > 1000)
+
+    # make sure we get the right row return type
+    assert(type(nodes[0]) == tuple)
+
+    # make sure each node is a row from edges
+    assert(len(nodes[0]) == 10)
+
+
+if __name__ == '__main__':
+
+    test_nodes_in_radius()
+    print("done.")
