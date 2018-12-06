565af6de9d7eca40a2f80d86cd16dbb39e75b204
Joshua Sennett <joshua.sennett@tufts.edu>
2018-12-05 20:53:44 -0500

    Improved performance of database utils

--- data/database_utils.py
+++ data/database_utils.py
@@ -1,17 +1,31 @@
 import psycopg2
 from psycopg2.extras import execute_values
+from geographiclib.geodesic import Geodesic
 
 
-def nodes_within_radius(credentials, lat=42.359055, lon=-71.093500, radius=.1, limit=250000):
+def edges_within_radius(credentials, lat=42.359055, lon=-71.093500, radius=1000, limit=None):
+    """
+    Return edges within a search radius around a point.
+    This will query the database, returning a list of edges
+    each with the relevant information for the edge.
 
-    # Make sure that user-input coordinates are valid
-    # if not ((-73.6 < lon < -69.8) and (41.0 > lat > 43.0)):
-    if lon < -73.6 or lon > -69.8 or lat < 41.0 or lat > 43.0:
-        print("Invalid coordinates; not in MA:", (lat, lon))
-        return []
+    @input
+    credentials: dict - database credentials
+    lat: float - latitude of midpoint
+    lon: float - longitude of midpoint
+    radius: float - radius, in meters
+    limit: int, or NoneType - number of rows to limit in query
+    """
+    # If no limit or negative limit, convert to limit to an unreachable limit
+    if limit is None or limit <= 0:
+        limit = 10000000 # larger than our table
 
     conn = psycopg2.connect(**credentials)
     c = conn.cursor()
+
+    # ST_DWithin uses SRID units (degrees), so we need to convert
+    # meters to approximate degrees (1 degree ~ 111,000 meters)
+    radius_in_degrees = radius / 111000.0
 
     results = []
     sql = """
@@ -23,13 +37,11 @@ def nodes_within_radius(credentials, lat=42.359055, lon=-71.093500, radius=.1, l
             WHERE ST_DWithin(start_node_coord, ST_SetSRID(ST_Point(%s, %s), 4326), %s)
             LIMIT %s;
           """
-    input = (lon, lat, radius, limit)
+    input = (lon, lat, radius_in_degrees, limit)
 
     try:
         c.execute(sql, input)
         results = c.fetchall()
-    except:
-        print("exception - query cancelled.")
     finally:
         c.close()
         conn.close()
@@ -38,34 +50,36 @@ def nodes_within_radius(credentials, lat=42.359055, lon=-71.093500, radius=.1, l
 
 
 def nearest_node(credentials, lat=42.359055, lon=-71.093500, meters=100):
-
-    # Make sure that user-input coordinates are valid
-    # if not ((-73.6 < lon < -69.8) and (41.0 > lat > 43.0)):
-    if lon < -73.6 or lon > -69.8 or lat < 41.0 or lat > 43.0:
-        print("Invalid coordinates; not in MA:", (lat, lon))
-        return ()
-
+    """
+    Note: it is slow (~1 min) to search for a the nearest node to a point
+    that does not exist in the database.
+    """
     conn = psycopg2.connect(**credentials)
     c = conn.cursor()
 
     results = () # initialize results, in case we are unable to fetch them
     sql = """
-            SELECT
-                start_node_id, start_node_lat, start_node_lon,
-                end_node_id, end_node_lat, end_node_lon,
-                distance, highway_group, bicycle_group, incline
+            SELECT start_node_id, start_node_lat, start_node_lon
             FROM edges
-            WHERE ST_Distance_Sphere(start_node_coord, ST_MakePoint(%s, %s)) <= %s
             ORDER BY start_node_coord <-> ST_SetSRID(ST_MakePoint(%s, %s),4326)
             LIMIT 1;
           """
-    input = (lon, lat, meters, lon, lat)
+    input = (lon, lat)
 
     try:
         c.execute(sql, input)
-        results = c.fetchone()
+        result = c.fetchone()
     finally:
         c.close()
         conn.close()
 
-    return results
+    # If the nearest node is further than our allowed distance,
+    # we cannot find a closest node. This ends up being faster
+    # than including a search radius within the SQL query
+    if result is not None:
+
+        dist = Geodesic.WGS84.Inverse(lat, lon, result[1], result[2])['s12']
+        if dist > meters:
+            return None
+
+    return result
